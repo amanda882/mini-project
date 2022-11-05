ef5140ddbdb7f02c98188c3cd96283013ae3e91c
Amanda Lang <alang@tb.yearup.org>
2022-11-05 12:58:34 -0400

    add arrays to js pags created html page

---
+++ food-menu.js
@@ -0,0 +1,26 @@
+// Create drop dfown category of menu items
+
+let drinks = 0;
+
+let menu = {
+drinks: ['Coke', 'RedBull', 'Peach Tea', 'Water', 'Sprite', 'Long Island Ice Tea', 'Dirty Shirley'],
+entrees: ['Double Cheese Burger w/ Fries', 'Bacon Cheese burger w/ Mac and Cheese', 'Spicy Chicken Sandwich w/ Curley fries', 'Cobb Salad w/ Ranch Dressing', 'Boneless Wings w/ tater Tots' ],
+desserts: ['Fried Cheesecake', 'Chocolate Cake', 'Cheesecake', 'Pumpkin Chocolate Chip Cookie', 'Icecream']
+};
+
+let selectMenu = document.getElementById('catagories');
+console.log(`the select menu is ${selectMenu}`);
+
+
+// drop down menu option
+let menuItems = menu.length;
+console.log(`number of menu items ${menuItems}`);
+
+
+window.onload = function() {
+    let menuDropDown = menu.length;
+    console.log(menuDropDown);
+for(let i = 0; i < length; i++);
+let drinkDrop = new Option(drinks[i].value );
+}
+
